3497e22d8a9dcf9c0777b6bc5b7c2a1d0cdd6050
AkashB45 <124855213+AkashB45@users.noreply.github.com>
2024-02-21 11:46:19 +0530

    Add files via upload

---
+++ 727722euit012/cc/grigo/src/main/java/com/example/grigo/GrigoApplication.java
@@ -0,0 +1,13 @@
+package com.example.grigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrigoApplication.class, args);
+	}
+
+}
